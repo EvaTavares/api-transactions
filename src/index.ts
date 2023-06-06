6c82dddb172fed2fe31f9b4b9e3587f03e86bbc8
Eva Tavares <eva.programadora@gmail.com>
2023-06-06 19:44:08 -0300

    configuracao para o deploy

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,30 @@
 import express, { NextFunction, Request, Response } from "express";
-import { users } from "./database/users";
 import { UserController } from "./controllers/user.controller";
-import { User } from "./models/user";
 import { TransactionController } from "./controllers/transactions.controller";
-import { StatusCodes } from "http-status-codes";
 import { UserMiddleware } from "./middleware/user.middleware";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Rota de usuários
-//listar usuários
+//Listar usuários
 app.get("/users", new UserController().getAllUsers);
 
-// listar por id
+// Listar por id
 app.get("/users/:id", new UserController().listUserId);
 
-// criar usuario
+// Criar usuário
 app.post("/users", new UserController().createUser);
 
-// atualizar usuário
+// Atualizar usuário
 app.put("/users/:id", new UserController().update);
 
-// deletar usuário
+// Deletar usuário
 app.delete("/users/:id", new UserController().delete);
 
 //Criar Transaction
@@ -32,30 +34,35 @@ app.post(
   new TransactionController().createTransactions
 );
 
+//Listar transactions
 app.get(
   "/user/:userId/transactions/:idTransaction",
   [UserMiddleware.validateUser],
   new TransactionController().listTransaction
 );
 
+//Listar Balance
 app.get(
   "/users/:userId/transactions",
   [UserMiddleware.validateUser],
   new TransactionController().listBalance
 );
 
+//Editar transaction
 app.put(
   "/user/:userId/transactions/:idTransaction",
   [UserMiddleware.validateUser],
   new TransactionController().updateBalance
 );
 
+//Deletar transaction
 app.delete(
   "/user/:userId/transactions/:idTransaction",
   [UserMiddleware.validateUser],
   new TransactionController().deleteTransaction
 );
 
-app.listen(3333, () => {
-  console.log("API is running");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`API is running on port ${PORT}`);
 });
